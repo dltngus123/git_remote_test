4fa2f438d79e4d1d4f27a05afd619a9000cfe83f
dlthgus123 <awsdr123@gmail.com>
2023-05-08 15:46:58 +0900

    첫 커밋

---
+++ src/main/java/com/lsh/exam/demo/vo/Article.java
@@ -0,0 +1,45 @@
+package com.lsh.exam.demo.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private int memberId;
+	private String title;
+	private String body;
+	private int hitCount;
+	private int goodReactionPoint;
+	private int badReactionPoint;
+	
+	private String extra__writerName;
+	private boolean extra__actorCanDelete;
+	private boolean extra__actorCanModify;
+	
+	public String getRegDateForPrint() {
+		return regDate.substring(2, 16);
+	}
+	
+	public String getUpdateDateForPrint() {
+		return updateDate.substring(2, 16);
+	}
+	
+	public String forPrintType1RegDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
+	}
+	
+	public String forPrintType1UpdateDate() {
+		return updateDate.substring(2, 16).replace(" ", "<br>");
+	}
+	
+	public String getForPtintBody() {
+		return body.replaceAll("\n", "<br>");
+	}
+}
